02ca8dece83f7b1a25ffd2c321faf594fef18761
Eric Rochester <erochest@gmail.com>
2025-01-30 13:45:21 -0500

    better test framework integration

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,88 +1,67 @@
 use std::fs;
 use std::process::Command;
-
-use serde::Deserialize;
-use toml;
+use std::path::Path;
 
 use assert_cmd::prelude::*;
-use assert_fs::prelude::*;
-use assert_fs::NamedTempFile;
 use pretty_assertions::assert_eq;
 
-static TEST_SUITE_FILE: &str = "tests/test_suite.toml";
+fn test_tardi_file(tardi_file: &Path) -> datatest_stable::Result<()> {
+    let output = Command::cargo_bin(env!["CARGO_PKG_NAME"])
+        .unwrap()
+        .arg("--print-stack")
+        .arg(tardi_file)
+        .output()
+        .unwrap();
 
-#[derive(Debug, Deserialize)]
-struct TestSuite {
-    #[serde(rename = "TestCase")]
-    test_cases: Vec<TestCase>
+    // Validate results
+    validate_status(tardi_file, &output);
+    validate_print_output(tardi_file, "stdout", &String::from_utf8_lossy(&output.stdout));
+    validate_print_output(tardi_file, "stderr", &String::from_utf8_lossy(&output.stderr));
+
+    Ok(())
 }
 
-#[derive(Debug, Deserialize)]
-struct TestCase {
-    name: String,
-    input: String,
-    status: Option<i32>,
-    stdout: Option<String>,
-    stderr: Option<String>,
-}
-
-#[test]
-fn test_run() {
-    let test_suite_spec = fs::read_to_string(TEST_SUITE_FILE).unwrap();
-    let test_suite: TestSuite = toml::from_str(&test_suite_spec).unwrap();
-
-    for test_case in test_suite.test_cases {
-        // TODO: better integrate these into the testing framework. Maybe make this a macro that reads the test suite at compile time?
-
-        // Create temporary file with .tardi extension
-        let temp_file = NamedTempFile::new("test.tardi").unwrap();
-        temp_file.write_str(&test_case.input).unwrap();
-        
-        // Run command and capture output
-        let output = Command::cargo_bin(env!["CARGO_PKG_NAME"])
+fn validate_status(tardi_file: &Path, output: &std::process::Output) {
+    let status_file = tardi_file.with_extension("status");
+    
+    let expected_status = if status_file.exists() {
+        fs::read_to_string(status_file)
             .unwrap()
-            .arg("--print-stack")
-            .arg(temp_file.path())
-            .output()
-            .unwrap();
-
-        // Validate results
-        validate_status(&test_case, &output);
-        validate_stdout(&test_case, &output);
-        validate_stderr(&test_case, &output);
-    }
+            .trim()
+            .parse::<i32>()
+            .unwrap()
+    } else {
+        0
+    };
+    
+    assert_eq!(
+        output.status.code().unwrap(),
+        expected_status,
+        "Test file '{}' failed. Error message: {}",
+        tardi_file.display(),
+        String::from_utf8_lossy(&output.stderr)
+    );
 }
 
-fn validate_status(test_case: &TestCase, output: &std::process::Output) {
-    if let Some(expected_status) = test_case.status {
+fn validate_print_output(tardi_file: &Path, extension: &str, expected_output: &str) {
+    let output_file = tardi_file.with_extension(extension);
+    
+    if output_file.exists() {
+        let expected = fs::read_to_string(output_file).unwrap();
         assert_eq!(
-            output.status.code().unwrap(),
-            expected_status,
-            "Test case '{}' status mismatch. Error message: {}",
-            test_case.name,
-            String::from_utf8_lossy(&output.stderr)
+            expected_output,
+            &expected,
+            "Test file '{}' {} mismatch",
+            tardi_file.display(),
+            extension
         );
     }
 }
 
-fn validate_stdout(test_case: &TestCase, output: &std::process::Output) {
-    if let Some(ref expected_stdout) = test_case.stdout {
-        assert_eq!(
-            &String::from_utf8_lossy(&output.stdout),
-            expected_stdout,
-            "Test case '{}' stdout mismatch",
-            test_case.name
-        );
+datatest_stable::harness! (
+    {
+        test = test_tardi_file,
+        root = "tests/fixtures",
+        pattern = r"^.*\.tardi$",
     }
-}
-
-fn validate_stderr(test_case: &TestCase, output: &std::process::Output) {
-    if let Some(ref expected_stderr) = test_case.stderr {
-        assert_eq!(
-            &String::from_utf8_lossy(&output.stderr),
-            expected_stderr,
-            "Test case '{}' stderr mismatch",
-            test_case.name
-        );
-    }
-}
+);
