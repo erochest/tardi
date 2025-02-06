86eb53d44183e95d582dc87654338b4a655967f5
Eric Rochester <erochest@gmail.com>
2025-02-06 08:14:44 -0500

    Added Default to VM

--- src/vm.rs
+++ src/vm.rs
@@ -1,57 +1,54 @@
-
-
 use crate::chunk::Chunk;
 use crate::error::{Error, Result};
 use crate::op_code::OpCode;
 use crate::value::Value;
 use std::convert::TryFrom;
 
+#[derive(Default)]
 pub struct VM {
     stack: Vec<Value>,
 }
 
 impl VM {
     pub fn new() -> Self {
-        VM {
-            stack: Vec::new(),
-        }
+        VM { stack: Vec::new() }
     }
 
     pub fn execute(&mut self, chunk: Chunk) -> Result<()> {
         let mut ip = 0;
-        
+
         while ip < chunk.code.len() {
             let instruction = chunk.code[ip];
-            
+
             match OpCode::try_from(instruction)? {
                 OpCode::GetConstant => {
                     ip += 1;
                     let constant_idx = chunk.code[ip];
                     let constant = chunk.constants[constant_idx as usize].clone();
                     self.stack.push(constant);
-                },
+                }
                 OpCode::Add => {
                     let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     self.stack.push((a + b)?);
-                },
+                }
                 OpCode::Sub => {
                     let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     self.stack.push((a - b)?);
-                },
+                }
                 OpCode::Mult => {
                     let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     self.stack.push((a * b)?);
-                },
+                }
                 OpCode::Div => {
                     let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
                     self.stack.push(a.checked_div(b)?);
-                },
+                }
             }
-            
+
             ip += 1;
         }
 
