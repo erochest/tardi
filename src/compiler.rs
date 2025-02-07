8c8e33d10d725b075e56ca27443a4a6fd3538283
Eric Rochester <erochest@gmail.com>
2025-02-07 07:36:55 -0500

    fixed issues

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,12 +1,12 @@
 use crate::chunk::Chunk;
+use crate::op_code::OpCode;
 use crate::parser::{Token, TokenType};
 use crate::value::Value;
-use crate::op_code::OpCode;
 
 pub fn compile(tokens: Vec<Token>) -> Chunk {
     let mut chunk = Chunk::new();
     let mut current = 0;
-    
+
     while current < tokens.len() {
         let token = &tokens[current];
         match &token.token_type {
@@ -14,35 +14,30 @@ pub fn compile(tokens: Vec<Token>) -> Chunk {
                 let constant = chunk.add_constant(Value::Integer(*number));
                 chunk.code.push(OpCode::GetConstant as u8);
                 chunk.code.push(constant as u8);
-            },
+            }
             TokenType::String(string) => {
                 let constant = chunk.add_constant(Value::String(string.clone()));
                 chunk.code.push(OpCode::GetConstant as u8);
                 chunk.code.push(constant as u8);
-            },
-            TokenType::String(string) => {
-                let constant = chunk.add_constant(Value::String(string.clone()));
-                chunk.code.push(OpCode::GetConstant as u8);
-                chunk.code.push(constant as u8);
-            },
+            }
             TokenType::Plus => {
                 chunk.code.push(OpCode::Add as u8);
-            },
+            }
             TokenType::Minus => {
                 chunk.code.push(OpCode::Sub as u8);
-            },
+            }
             TokenType::Multiply => {
                 chunk.code.push(OpCode::Mult as u8);
-            },
+            }
             TokenType::Division => {
                 chunk.code.push(OpCode::Div as u8);
-            },
+            }
         }
         current += 1;
     }
-    
+
     chunk
-} 
+}
 
 #[cfg(test)]
 mod tests;
--- src/compiler/tests.rs
+++ src/compiler/tests.rs
@@ -1,42 +1,85 @@
+use std::convert::TryInto;
+
 use pretty_assertions::assert_eq;
 
-use crate::error::Result;
-use crate::token::{Token, TokenType};
+use crate::parser::{Token, TokenType};
 
 use super::*;
 
 fn test_compile(input: Vec<TokenType>, expected: Vec<u8>) {
-    let tokens = input.into_iter().map(|tt| Token { token_type: tt, line_no: 1 }).collect();
+    let tokens = input
+        .into_iter()
+        .map(|tt| Token {
+            token_type: tt,
+            line_no: 1,
+            column: 0,
+            length: 3,
+        })
+        .collect();
     let chunk = compile(tokens);
     assert_eq!(chunk.code, expected);
 }
 
 #[test]
 fn test_compile_simple_expression_with_div() {
-    test_compile(vec!["10".try_into().unwrap(), "3".try_into().unwrap(), "/".try_into().unwrap()], vec![0, 0, 0, 1, OpCode::Div as u8]);
+    test_compile(
+        vec![
+            "10".try_into().unwrap(),
+            "3".try_into().unwrap(),
+            "/".try_into().unwrap(),
+        ],
+        vec![0, 0, 0, 1, OpCode::Div as u8],
+    );
 }
 
 #[test]
 fn test_compile_simple_expression() {
-    test_compile(vec!["10".try_into().unwrap(), "3".try_into().unwrap()], vec![0, 0, 0, 1]);
+    test_compile(
+        vec!["10".try_into().unwrap(), "3".try_into().unwrap()],
+        vec![0, 0, 0, 1],
+    );
 }
 
 #[test]
 fn test_compile_simple_expression_with_add() {
-    test_compile(vec!["10".try_into().unwrap(), "3".try_into().unwrap(), "+".try_into().unwrap()], vec![0, 0, 0, 1, OpCode::Add as u8]);
+    test_compile(
+        vec![
+            "10".try_into().unwrap(),
+            "3".try_into().unwrap(),
+            "+".try_into().unwrap(),
+        ],
+        vec![0, 0, 0, 1, OpCode::Add as u8],
+    );
 }
 
 #[test]
 fn test_compile_simple_expression_with_sub() {
-    test_compile(vec!["10".try_into().unwrap(), "3".try_into().unwrap(), "-".try_into().unwrap()], vec![0, 0, 0, 1, OpCode::Sub as u8]);
+    test_compile(
+        vec![
+            "10".try_into().unwrap(),
+            "3".try_into().unwrap(),
+            "-".try_into().unwrap(),
+        ],
+        vec![0, 0, 0, 1, OpCode::Sub as u8],
+    );
 }
 
 #[test]
 fn test_compile_simple_expression_with_mult() {
-    test_compile(vec!["10".try_into().unwrap(), "3".try_into().unwrap(), "*".try_into().unwrap()], vec![0, 0, 0, 1, OpCode::Mult as u8]);
+    test_compile(
+        vec![
+            "10".try_into().unwrap(),
+            "3".try_into().unwrap(),
+            "*".try_into().unwrap(),
+        ],
+        vec![0, 0, 0, 1, OpCode::Mult as u8],
+    );
 }
 
 #[test]
 fn test_compile_string() {
-    test_compile(vec!["\"hello\"".try_into().unwrap()], vec![OpCode::GetConstant as u8, 0]);
+    test_compile(
+        vec!["\"hello\"".try_into().unwrap()],
+        vec![OpCode::GetConstant as u8, 0],
+    );
 }
