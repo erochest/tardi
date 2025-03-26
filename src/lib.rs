60fc242f0bb35b7813394564111e7ec73700946e
Eric Rochester <erochest@gmail.com>
2025-03-26 07:38:55 -0400

    Refactor: Move run_file function from main.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,24 @@ pub use error::Result;
 pub use scanner::Scanner;
 pub use vm::value::Value;
 pub use vm::VM;
+
+/// Run a Tardi source file
+pub fn run_file(path: &PathBuf, print_stack: bool) -> Result<()> {
+    let source = std::fs::read_to_string(path)?;
+    let scanner = Scanner::new(&source);
+    let mut compiler = Compiler::new();
+    let program = compiler.compile(scanner)?;
+
+    let mut vm = VM::new();
+    vm.load_program(Box::new(program));
+    vm.run()?;
+
+    if print_stack {
+        // Print stack contents from top to bottom
+        for value in vm.stack_iter() {
+            eprintln!("{}", value);
+        }
+    }
+
+    Ok(())
+}
