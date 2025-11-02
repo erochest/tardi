3d73be227ed15619e6c743dfbbe349d135d54715
Eric Rochester <erochest@gmail.com>
2025-11-02 18:36:05 -0500

    tests to do

--- tardi-scanner/src/tests/integers.rs
+++ tardi-scanner/src/tests/integers.rs
@@ -140,3 +140,73 @@ fn test_scans_negative_integers() {
     assert_isize(-4255, &values[3]);
     assert_isize(-50101, &values[4]);
 }
+
+#[test]
+#[ignore]
+fn test_scans_positive_signs() {
+    let scanner = Scanner::from_string("+1 +20 +350 +4255 +50101");
+    let values = scanner.collect::<Vec<_>>();
+    assert_isize(1, &values[0]);
+    assert_isize(20, &values[1]);
+    assert_isize(350, &values[2]);
+    assert_isize(4255, &values[3]);
+    assert_isize(50101, &values[4]);
+}
+
+// TODO: placeholders: 1_000_000 1,000,001
+#[test]
+#[ignore]
+fn test_scans_delimited_integers() {
+    let scanner = Scanner::from_string("1_000_000 1,000,001");
+    let values = scanner.collect::<Vec<_>>();
+    assert_isize(1000000, &values[0]);
+    assert_isize(1000001, &values[1]);
+}
+
+// TODO: hexadecimal: 0x66 0xff
+#[test]
+#[ignore]
+fn test_scans_hexadecimal_integers() {
+    let scanner = Scanner::from_string("0x66 0xff");
+    let values = scanner.collect::<Vec<_>>();
+    assert_isize(102, &values[0]);
+    assert_isize(255, &values[1]);
+}
+
+// TODO: octal: 0o66 0o32
+#[test]
+#[ignore]
+fn test_scans_octal_integers() {
+    let scanner = Scanner::from_string("0o66 0o32");
+    let values = scanner.collect::<Vec<_>>();
+    assert_isize(54, &values[0]);
+    assert_isize(26, &values[1]);
+}
+
+// TODO: binary: 0b10 0b1111
+#[test]
+#[ignore]
+fn test_scans_binary_integers() {
+    let scanner = Scanner::from_string("0b10 0b1111");
+    let values = scanner.collect::<Vec<_>>();
+    assert_isize(2, &values[0]);
+    assert_isize(15, &values[1]);
+}
+
+// TODO: type markers, positive, negative, and placeholders: 1:u8 20:u32 4524:usize -1:i8 -20:i32 -4524:i64 1_000_000:i64 1,000,001:u64
+#[test]
+#[ignore]
+fn test_scans_typed_numbers() {
+    let scanner = Scanner::from_string(
+        "1:u8 20:u32 4524:usize -1:i8 -20:i32 -4524:i64 1_000_000:i64 1,000,001:u64",
+    );
+    let values = scanner.collect::<Vec<_>>();
+    assert_u8(1, &values[0]);
+    assert_u32(20, &values[1]);
+    assert_usize(4524, &values[2]);
+    assert_i8(-1, &values[3]);
+    assert_i32(-20, &values[4]);
+    assert_i64(-4524, &values[5]);
+    assert_i64(1000000, &values[6]);
+    assert_u64(1000001, &values[7]);
+}
